bc983dbbb846d3099599b534c4880a9cbd9dbf5f
Ming-der Wang <mingderwang@gmail.com>
2016-11-23 00:11:32 +0800

    change banner

--- s3sync.sh
+++ s3sync.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
-MAIN_DIR=/Users/mwang/src/zone/61/log4analytics.com/mingderwang.github.io.source
-SRC_DIR=/Users/mwang/src/zone/61/log4analytics.com/mingderwang.github.io.source/_site
-DES_DIR=/Users/mwang/src/zone/61/log4analytics.com/www-log4analytics-com
+MAIN_DIR=/Users/mingderwang/src/web/mingderwang.github.io.source
+#/Users/mwang/src/zone/61/log4analytics.com/mingderwang.github.io.source
+SRC_DIR=/Users/mingderwang/src/web/mingderwang.github.io.source/_site
+#/Users/mwang/src/zone/61/log4analytics.com/mingderwang.github.io.source/_site
+DES_DIR=/Users/mingderwang/src/web/www.log4analytics.com
+#/USERS/MWANG/SRC/ZONE/61/LOG4ANALYTICS.COM/WWW-LOG4ANALYTICS-COM
 
 jekyll build
 echo '----------'
@@ -11,7 +14,7 @@ cd $MAIN_DIR
 #bundle exec jekyll build
 rm -r $DES_DIR
 mkdir $DES_DIR
-rcp -r $SRC_DIR/* $DES_DIR
+cp -r $SRC_DIR/* $DES_DIR
 cd $DES_DIR
 rm *.sh
 s3cmd sync . s3://www.log4analytics.com
