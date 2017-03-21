c52192fb7fb3d0306ae59cde543d6dfe44877b52
Ming-der Wang <ming@log4analytics.com>
2017-03-21 12:47:30 +0800

    change local path

--- s3sync.sh
+++ s3sync.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-MAIN_DIR=/Users/mingderwang/src/web/mingderwang.github.io.source
+MAIN_DIR=/Users/mingderwang/2016/src/zone/mingderwang.github.io.source
+#/Users/mingderwang/src/web/mingderwang.github.io.source
 #/Users/mwang/src/zone/61/log4analytics.com/mingderwang.github.io.source
-SRC_DIR=/Users/mingderwang/src/web/mingderwang.github.io.source/_site
+SRC_DIR=/Users/mingderwang/2016/src/zone/mingderwang.github.io.source/_site
 #/Users/mwang/src/zone/61/log4analytics.com/mingderwang.github.io.source/_site
-DES_DIR=/Users/mingderwang/src/web/www.log4analytics.com
+DES_DIR=/Users/mingderwang/2016/src/zone/www.log4analytics.com
 #/USERS/MWANG/SRC/ZONE/61/LOG4ANALYTICS.COM/WWW-LOG4ANALYTICS-COM
 
 bundle exec jekyll build
